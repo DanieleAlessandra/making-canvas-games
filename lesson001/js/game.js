4e97f995e1f4c9188203bc7ca69f101d4c1c7c72
Daniele Alessandra <bitbucket@danielealessandra.com>
2016-11-06 00:05:30 +0100

    Added update and draw Methods

--- lesson001/js/game.js
+++ lesson001/js/game.js
@@ -6,14 +6,25 @@
         canvasContext: undefined
     };
     
+    App.draw = function () {
+        /// Qui si disegnano gli elementi
+        document.write('Draw ');
+    };
+    
     App.enterFrame = function () {
-        document.write('. ');
+        App.update();
+        App.draw();
     };
     
     App.init = function () {
         App.canvasElement = document.getElementById("gameCanvas");
         App.canvasContext = App.canvasElement.getContext("2d");
         setInterval(App.enterFrame, 1000 / 60);
+    };
+    
+    App.update = function () {
+        /// Qui si aggiornano gli elementi
+        document.write('Update ');
     };
     
     document.addEventListener('DOMContentLoaded', App.init);
