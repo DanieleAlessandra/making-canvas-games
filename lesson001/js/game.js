6df12a682614b049977502da45683db4d82d2e20
Daniele Alessandra <bitbucket@danielealessandra.com>
2016-11-05 23:35:05 +0100

    Added enterFrame Method

--- lesson001/js/game.js
+++ lesson001/js/game.js
@@ -6,9 +6,14 @@
         canvasContext: undefined
     };
     
+    App.enterFrame = function () {
+        document.write('. ');
+    };
+    
     App.init = function () {
         App.canvasElement = document.getElementById("gameCanvas");
         App.canvasContext = App.canvasElement.getContext("2d");
+        setInterval(App.enterFrame, 1000 / 60);
     };
     
     document.addEventListener('DOMContentLoaded', App.init);
