534b1efac0069ecc5477474c82126e200e2dc3a8
Daniele Alessandra <bitbucket@danielealessandra.com>
2016-11-05 23:15:43 +0100

    Added DOMContentLoaded Listener

--- lesson001/js/game.js
+++ lesson001/js/game.js
@@ -6,4 +6,11 @@
         canvasContext: undefined
     };
     
+    App.init = function () {
+        App.canvasElement = document.getElementById("gameCanvas");
+        App.canvasContext = App.canvasElement.getContext("2d");
+    };
+    
+    document.addEventListener('DOMContentLoaded', App.init);
+    
 }());
